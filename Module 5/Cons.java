2d711a536cf7a112106ec91c17df9f3bda7dd4f5
MOCmaniac <52349445+MOCmaniac@users.noreply.github.com>
2019-11-07 16:52:25 +0100

    Add files via upload

---
+++ Module 5/Cons.java
@@ -0,0 +1,40 @@
+import java.util.function.Predicate;
+import java.util.function.Function;
+
+public class Cons<E> {
+    // the item inside this list node
+    public E v;
+    // The next element, null if nothing
+    public Cons<E> next;
+    // Constructor
+    public Cons(E v, Cons<E> next) {
+        this.v = v;
+        this.next = next;
+    }
+
+    public <R> Cons <R> map(Function <E,R> function) {
+      E mappedValue = (E) function.apply(this.v);
+      Cons mappedCons = new Cons(mappedValue, null);
+      if(this.next != null) {
+          mappedCons.next = this.next.map(function);
+      }
+      return mappedCons;
+    }
+
+    public Cons <E> filter(Predicate <E> predicate) {
+        Cons filtered = new Cons(null, null);
+        Cons head = this;
+        while(head != null){
+            E headValue = (E) head.v;
+            if(predicate.test(headValue)){
+                filtered.v = head.v;
+                if(head.next != null){
+                    filtered.next = head.next.filter(predicate);
+                }
+                return filtered;
+            }
+            head = head.next;
+        }
+        return null;
+    } 
+}
---
+++ Module 5/Evaluator.java
@@ -0,0 +1,35 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+public class Evaluator {
+
+    public BiFunction<Boolean, Boolean, Boolean> xor_gate() {
+         return (a, b) -> a^b; // a^b is equivalent to a != b in this case
+    }
+
+    public BiFunction<Boolean, Boolean, Boolean> or_gate() {
+        return (a, b) -> a || b;
+    }
+
+    public BiFunction<Boolean, Boolean, Boolean> and_gate() {
+        return (a, b) -> a && b;
+    }
+
+    public Function<Boolean, Boolean> not_gate() {
+        return a -> !a;
+    }
+
+    // Should return a map with the results stored in map ( use HashMap )
+    // Keys are "SUM", "CarryOut"
+    // TODO WARNING : USE HERE ONLY the previously defined method to compute result (as inginious will prevent you to cheat to directly invoke logical operators)
+    public Map<String, Boolean> evaluate_circuit(Boolean a, Boolean b, Boolean carry_in) {
+        HashMap result = new HashMap();
+        boolean resultXOR1 = xor_gate().apply(a, b);
+        result.put("SUM", xor_gate().apply(resultXOR1, carry_in));
+        result.put("CarryOut", or_gate().apply(and_gate().apply(resultXOR1, carry_in), and_gate().apply(a, b)));
+        return result;
+    }
+
+}
