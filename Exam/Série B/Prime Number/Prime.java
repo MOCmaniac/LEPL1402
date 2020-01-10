5eac3564571256e98713621ef3f1912cf3815c82
Simetje <50112825+Simetje@users.noreply.github.com>
2020-01-10 14:21:38 +0100

    OnlyWithStreamsPrime
    Prime but only using java IntStream class.

--- Exam/Série B/Prime Number/Prime.java
+++ Exam/Série B/Prime Number/Prime.java
@@ -4,25 +4,23 @@ public class Prime {
 
     // Suppose x has 2 factors (n1 and n2) and both are bigger than sqrt(x). Then n1*n2 is bigger than x, which is absurd!
     // So x has at least one factor smaller than sqrt(x) if it isn’t prime. That’s what we want to find!
-    public static boolean isPrime(Integer x) {
-        if(x < 2)
-            return false;
-        for (int i = 2; i <= Math.sqrt(x); i++) {
-            if (x % i == 0)
-                return false;
-        }
-        return true;
+    public static boolean isPrime(int number) {
+        return IntStream.rangeClosed(1,(int) Math.sqrt(number)).filter(d -> number%d == 0).count() == 1 && number > 1;
     }
 
-    public static IntStream infiniteStream(Integer from) {
-        return IntStream.iterate(from, i -> i += 1);
+    public static IntStream infiniteStream(int from) {
+        return IntStream.iterate(from, i -> i+1);
     }
 
-    public static IntStream getPrimeStream(Integer from) {
+    public static IntStream getPrimeStream(int from) {
         return infiniteStream(from).filter(Prime::isPrime);
     }
 
-    public static IntStream getPrimeStreamGap(Integer from) {
+    public static IntStream getPrimeStreamGap(int from) {
         return getPrimeStream(from).map(i -> getPrimeStream(i+1).limit(1).sum()-i);
+    }
+
+    public static void main(String[] args) {
+        getPrimeStream(0).limit(20).forEach(System.out::println);
     }
 }
