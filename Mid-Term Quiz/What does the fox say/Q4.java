0270b7673a43338d67789fa348fa243611b40b5c
MOCmaniac <52349445+MOCmaniac@users.noreply.github.com>
2021-10-21 12:11:29 +0200

    Create Q4.java

---
+++ Mid-Term Quiz/What does the fox say/Q4.java
@@ -0,0 +1,8 @@
+// This is in another file
+/*
+public class Q4 {
+    static public void printHeight(Animal animal) {
+        System.out.println(animal.height);
+        // Doesn't compile because the variable "height" isn't defined in the class animal
+    }
+}*/
